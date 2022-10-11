f7c48cebbcb51e30ab9f5844b487e2ea4adb5fe4
Saif Adel <saifadel670@gmail.com>
2022-10-12 03:22:25 +0600

    placeholder and constant added

---
+++ iOS-Practical-Test/Shared/Constants/ApiConstant.swift
@@ -0,0 +1,13 @@
+//
+//  ApiConstant.swift
+//  iOS-Practical-Test
+//
+//  Created by Saif Adel on 12/10/22.
+//
+
+import Foundation
+
+public let KEY = "38e61227f85671163c275f9bd95a8803"
+public let APIKEY = "api_key"
+public let QUERY = "query"
+
---
+++ iOS-Practical-Test/Shared/Constants/StringConstant.swift
@@ -0,0 +1,14 @@
+//
+//  StringConstant.swift
+//  iOS-Practical-Test
+//
+//  Created by Saif Adel on 12/10/22.
+//
+
+import Foundation
+
+enum StringConstant: String {
+    case marvel = "marvel"
+    case posterPlaceHolderPath = "poster_placeholder"
+}
+
