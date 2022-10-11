cd509769b550ef09f86290c7859fd892fffad39e
Saif Adel <saifadel670@gmail.com>
2022-10-12 01:46:14 +0600

    Initial Commit

---
+++ iOS-Practical-Test/iOS_Practical_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Practical_TestApp.swift
+//  iOS-Practical-Test
+//
+//  Created by Saif Adel on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Practical_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
