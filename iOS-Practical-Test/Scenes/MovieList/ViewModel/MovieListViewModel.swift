7fffbf2e34c35630679516890c0a111d5c311cdd
Saif Adel <saifadel670@gmail.com>
2022-10-12 03:23:40 +0600

    viewModel added and data fetched

---
+++ iOS-Practical-Test/Scenes/MovieList/ViewModel/MovieListViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  MovieListViewModel.swift
+//  iOS-Practical-Test
+//
+//  Created by Saif Adel on 12/10/22.
+//
+
+import Foundation
+
+
+class MovieListViewModel: ObservableObject {
+    @Published var movies = MovieListModel.Response.init( results: [])
+    @Published var loading = false
+    var param : String = ""
+    
+    init() {
+        loading = true
+        fetchMovieList()
+    }
+    
+    public func fetchMovieList(searchQuery : String = StringConstant.marvel.rawValue) {
+        guard var url = URL.moviePath(searchQuery) else {
+            fatalError("URl was incorrect")
+        }
+        
+       
+        URLSession.shared.dataTask(with: url){ (data, _, _) in
+            guard let data = data else { return }
+            do{
+                let movies : MovieListModel.Response = try JSONDecoder().decode(MovieListModel.Response.self, from: data)
+                DispatchQueue.main.async {
+                    print("response:- \(url)")
+                    self.movies = movies
+                    self.loading = false
+                }
+            }
+            catch(let e) {
+                print("exceptin:- \(e.localizedDescription)")
+            }
+        }.resume()
+    }
+}
