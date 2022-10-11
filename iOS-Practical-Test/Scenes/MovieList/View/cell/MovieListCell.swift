73dd908e2d7148ff7d5470b22105e4090b43e240
Saif Adel <saifadel670@gmail.com>
2022-10-12 03:34:08 +0600

    List cell added

---
+++ iOS-Practical-Test/Scenes/MovieList/View/cell/MovieListCell.swift
@@ -0,0 +1,51 @@
+//
+//  MovieListCell.swift
+//  iOS-Practical-Test
+//
+//  Created by Saif Adel on 12/10/22.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct MovieListCell : View {
+    var movie: MovieListModel.Results
+    
+    let placeHolder : some View =
+    Image(StringConstant.posterPlaceHolderPath.rawValue)
+            .resizable()
+            .frame(width: 90, height: 120)
+            .clipShape(RoundedRectangle(cornerRadius: 25))
+    
+    
+    var body: some View {
+        HStack {
+            if let _ = movie.poster_path {
+                if let url = URL.posterUrl(movie.poster_path!) {
+                    AsyncImage(url: url) { image in
+                        image.resizable()
+                    } placeholder: {
+                        placeHolder
+                    }
+                    .frame(width: 90, height: 120)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                }
+            }
+            else {
+                placeHolder
+            }
+           
+                           
+            
+            
+            VStack (alignment: .leading){
+                    Text(movie.title!)
+                    Spacer()
+                    Text(movie.overview!)
+                Spacer()
+            }
+        }.frame(height: 130)
+    }
+}
+
